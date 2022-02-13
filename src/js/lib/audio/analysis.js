7c4c38b06b8b952ff7834782b9eaee879904a830
Alec Elton <alec.elton@gmail.com>
2022-02-13 10:13:28 -0600

    Rework - simple analysis only

--- src/js/lib/audio/analysis.js
+++ src/js/lib/audio/analysis.js
@@ -1,8 +1,4 @@
-import {FEATURES} from '/src/js/lib/constants';
 import * as events from '/src/js/lib/events';
-
-import 'Meyda';
-import {RealTimeBPMAnalyzer} from 'realtime-bpm-analyzer';
 
 export class AudioConsumer {
     constructor() {
@@ -13,264 +9,124 @@ export class AudioConsumer {
     stop(event, input) {}
 }
 
-// TEMP TESTING
-// class BeatDetector extends AudioConsumer {
-//     constructor() {
-//         super();
-//         this.min_tempo = 60;
-//         this.max_tempo = 160;
-//         this.buffer_size = 1024; // TODO: must come from the analyzer as it's the same
-//         this.sample_rate = 44100; // TODO: must come from the analyzer as it's the same
-//         this.min_beats = 2;
-//         this.window_size_s = ((this.min_tempo * this.min_beats) / 60) * 1.1;
-//         this.window_size_samples = Math.floor((this.window_size_s * this.sample_rate) / this.buffer_size) * this.buffer_size;
-//         this.threshold = 0.95;
-//         // this.freq_cutoff = 350;
-//         this.freq_cutoff = 200;
-
-//         this.max_bin = Math.ceil(this.freq_cutoff / ((this.sample_rate / 2) / (this.buffer_size / 2)));
-
-//         this.window = null;
-
-//         // console.log(this);
-//     }
-
-//     trim() {
-//         while (this.window.length > this.window_size_chunks)
-//             this.window.shift();
-//     }
-
-//     process(data) {
-//         if (this.window === null) return;
-//         let now = (new Date()).getTime();
-//         let samples = data.inputBuffer.getChannelData(0);
-//         this.window.copyWithin(0, this.buffer_size, this.window.length);
-//         this.window.set(samples, this.window.length - samples.length);
-
-//         // let peak = 0;
-//         // for (let i = 0; i < this.max_bin; i++) {
-//         //     peak = Math.max(peak, data[i]);
-//         // }
-//         // this.window.push([(new Date()).getTime(), peak]);
-//         // this.trim();
-
-//         // TODO: will include init 0s in the calculation, this is wrong until window_size_s is elapsed
-//         let avg = this.window.reduce((p, c) => p + c) / this.window.length;
-//         let min = this.window.reduce((p, c) => Math.min(p, c));
-//         let max = this.window.reduce((p, c) => Math.max(p, c), min);
-
-//         let thresh = this.threshold * avg;
-//         let peaks = [];
-//         // let diff = this.sample_rate / 500;
-//         for (let i = 0; i < this.window.length; i++) {
-//             // if (this.window[i] > thresh) {
-//             // if (this.window[i] > avg) {
-//             // if (this.window[i] > (0.95 * max)) {
-//             // if (this.window[i] > avg * 1.25) {
-//             // if (i - diff > 0 && this.window[i - diff] / this.window[i] < 0.1) {
-//             if (((this.window[i] - min) / (max - min)) > 0.8) {
-//                 peaks.push(i);
-//                 i += Math.floor(this.sample_rate / 4);
-//             }
-//         }
-
-//         // let avg = 0, max = 0;
-//         // this.window.forEach(v => { avg += v[1]; max = Math.max(max, v[1]); });
-//         // avg /= this.window.length;
-
-//         // let peaks = [], thresh = this.threshold * avg;
-//         // this.window.forEach(v => (v[1] > thresh) && peaks.push(v[0]));
-
-//         let dists = [];
-//         for (let i = 0; i < peaks.length; i++) {
-//             if (i > 0) {
-//                 let dist = (peaks[i] - peaks[i - 1]) / this.sample_rate;
-//                 dists.push(Math.round((dist + Number.EPSILON) * 10) / 10);
-//             }
-//         }
-//         dists.sort((a, b) => a - b);
-
-//         console.log(dists);
-//     }
-
-//     start(event, input) {
-//         this.window = new Float32Array(this.window_size_samples);
-
-//         // this.filter = input.context.createBiquadFilter();
-//         // this.filter.type = 'lowpass';
-//         // this.filter.frequency = this.freq_cutoff;
-//         this.filter = new BiquadFilterNode(input.context, {type: 'lowpass', frequency: this.freq_cutoff});
-//         input.source.connect(this.filter);
-
-//         this.processor = input.context.createScriptProcessor(this.buffer_size, 1, 1);
-//         this.processor.onaudioprocess = this.process.bind(this);
-//         this.filter.connect(this.processor);
-//         this.processor.connect(input.context.destination);
-//     }
-
-//     stop(event, input) {
-//         this.processor.disconnect(input.context.destination);
-//         this.filter.disconnect(this.processor);
-//         input.source.disconnect(this.filter);
-//         this.filter = this.processor = this.window = null;
-//     }
-// }
-
-class BeatDetector extends AudioConsumer {
-    constructor() {
-        super();
-        // this.min_tempo = 60;
-        // this.max_tempo = 160;
-        this.buffer_size = 8192; // TODO: must come from the analyzer as it's the same
-        // this.sample_rate = 44100; // TODO: must come from the analyzer as it's the same
-        // this.min_beats = 2;
-        // this.window_size_s = ((this.min_tempo * this.min_beats) / 60) * 1.1;
-        // this.window_size_samples = Math.floor((this.window_size_s * this.sample_rate) / this.buffer_size) * this.buffer_size;
-        // this.threshold = 0.95;
-        // // this.freq_cutoff = 350;
-        // this.freq_cutoff = 200;
-
-        // this.max_bin = Math.ceil(this.freq_cutoff / ((this.sample_rate / 2) / (this.buffer_size / 2)));
-
-        // this.window = null;
-
-        // // console.log(this);
+class ExpFilter {
+    constructor(val, alpha_decay, alpha_rise) {
+        this.val = val;
+        this.alpha_decay = alpha_decay || 0.5;
+        this.alpha_rise = alpha_rise || 0.5;
     }
 
-    process(data) {
-        this.analyzer.analyze(data);
+    update_one(vOld, vNew) {
+        let alpha = vNew > vOld ? this.alpha_rise : this.alpha_decay;
+        return alpha * vNew + (1 - alpha) * vOld;
     }
 
-    start(event, input) {
-        this.analyzer = new RealTimeBPMAnalyzer({
-            scriptNode: {
-                bufferSize: this.bufferSize
-            },
-            pushTime: 2000,
-            pushCallback: (err, bpm) => {
-                console.error(err);
-                console.log('bpm', bpm);
-            }
-        });
-        this.processor = input.context.createScriptProcessor(this.buffer_size, 1, 1);
-        this.processor.onaudioprocess = this.process.bind(this);
-        input.source.connect(this.processor);
-        this.processor.connect(input.context.destination);
-    }
-
-    stop(event, input) {
-        this.processor.disconnect(input.context.destination);
-        input.source.disconnect(this.processor);
-        this.processor = this.analyzer = null;
+    update(val, subkey) {
+        if (typeof val === 'object') {
+            Object.keys(val).forEach(k => {
+                let vNew = val[k];
+                if (subkey) vNew = vNew[subkey];
+                this.val[k] = this.update_one(this.val[k], vNew);
+            });
+        } else if (typeof val === 'array') {
+            val.forEach((v, i) => {
+                this.val[i] = this.update_one(this.val[i], v);
+            });
+        } else {
+            this.val = this.update_one(this.val, val);
+        }
+        return this.val;
     }
 }
-
-// let bd = new BeatDetector();
-
-let l = Math.floor(43 * 2.2);
-let v = [], t = [];
-for (let i = 0; i < l; i++) {
-    v.push(0);
-}
-let ign = 0;
-let intv = null;
 
 export class AudioAnalyzer extends AudioConsumer {
     constructor() {
         super();
+        this.filter = null;
         this.analyzer = null;
-        this.bufferSize = 1024;
-        this.mfccCoefficients = 13;
-        this.features = Object.keys(FEATURES).filter(f => f !=='_default');
+        this.interval = null;
+
+        this.fftSize = 2048;
+        this.fps = 60;
+
+        this.freqGroups = {
+            sub: [0, 60],
+            bass: [60, 250],
+            low_mid: [250, 500],
+            mid: [500, 2000],
+            high_mid: [2000, 4000],
+            presence: [4000, 6000],
+            brilliance: [6000, 20000],
+        };
+
+        this.samplesLen = parseInt(this.fps * 2.2);
+        this._resetProps();
+    }
+
+    _resetProps() {
+        this.analyzer = this.buffer = this.interval = this.sampleRate = this.freqPerBin = this.fGain = this.fSmooth = null;
+        this.samples = {};
+        Object.keys(this.freqGroups).forEach(k => this.samples[k] = {samples: [], ignorePeaks: 0});
     }
 
     start(event, input) {
-        let last_log = (new Date()).getTime(), count = 0;
-        events.fire('audio/analysis/features', this, {input, sampleRate: input.context.sampleRate, bufferSize: this.bufferSize, features: this.features});
-        this.analyzer = Meyda.createMeydaAnalyzer({
-            audioContext: input.context,
-            source: input.source,
-            bufferSize: this.bufferSize,
-            numberOfMFCCCoefficients: this.mfccCoefficients,
-            featureExtractors: this.features,
-            callback: (features) => {
-                count++;
-                if ((new Date()).getTime() - last_log > 1000) {
-                    console.log(features);
-                    console.log(count);
-                    last_log = (new Date()).getTime();
-                    count = 0;
-                }
-                events.fire('audio/analysis/data', this, features);
+        this.sampleRate = input.context.sampleRate;
+        this.freqPerBin = ((this.sampleRate / 2) / (this.fftSize / 2));
+        this.analyzer = new AnalyserNode(input.context, {fftSize: this.fftSize});
+        this.buffer = new Uint8Array(this.analyzer.frequencyBinCount);
+        input.source.connect(this.analyzer);
+        this.interval = window.setInterval(this.process.bind(this), 1 / this.fps);
 
-                let p = 0;
-                for (let i = 0; i < 5; i ++) {
-                    p = Math.max(p, features.amplitudeSpectrum[i]);
-                }
-                v.push(p);
-                v.shift();
-                if (ign > 0) {
-                    ign--;
-                    return;
-                }
-                let max = v.reduce((p, c) => Math.max(p, c));
-                let min = v.reduce((p, c) => Math.min(p, c));
-                // console.log((p - min) / (max - min));
-                let is_beat = false;
-                if ((p - min) / (max - min) > 0.8) {
-                    console.log('onset?');
-                    is_beat = true;
-                    ign = 4;
-                    t.push((new Date()).getTime());
-                }
-                t = t.filter(n => n > (new Date()).getTime() - 7000);
+        let temp = {};
+        Object.keys(this.freqGroups).forEach(k => temp[k] = 0);
+        this.fGain = new ExpFilter(temp, 0.01, 0.99);
 
-                let dists = t
-                    .map((n, i) => i > 0 ? n - t[i - 1] : null)
-                    .filter(n => n !== null && n <= 1000 && n >=300)
-                    .map(n => Math.floor(n / 10) * 10);
+        temp = {};
+        Object.keys(this.freqGroups).forEach(k => temp[k] = 0);
+        this.fSmooth = new ExpFilter(temp, 0.5, 0.99);
+    }
 
-                // dists.sort((a, b) => a - b);
-                let out = {};
-                dists.map(n => {
-                    out[n] = out[n] || 0;
-                    out[n]++;
-                });
-
-                out = Object.entries(out).filter(n => n[1] >= 2);
-                if (out.length) {
-                    out.sort((a, b) => a > b ? 1 : (a < b ? -1 : 0));
-                    // console.log('bpm', 60000 / parseInt(out[0][0]));
-                    if (is_beat) {
-                        if (intv)
-                            window.clearInterval(intv);
-                        intv = window.setInterval(function() { console.log('beat'); }, parseInt(out[0][0]));
-                    }
+    process() {
+        this.analyzer.getByteFrequencyData(this.buffer);
+        // 60-250hz
+        let bins = {};
+        this.buffer.forEach((v, i) => {
+            let lfreq = this.freqPerBin * i;
+            for (let key in this.freqGroups) {
+                if (lfreq >= this.freqGroups[key][0] && lfreq < this.freqGroups[key][1]) {
+                    bins[key] = bins[key] || [];
+                    bins[key].push(v / 255);
                 }
-
-                // console.log(dists);
-            },
+            }
         });
-        this.analyzer.start();
+        for (let key in bins) {
+            let max = bins[key].reduce((p, c) => p > c ? p : c);
+            let avg = bins[key].reduce((p, c) => p + c) / bins[key].length;
+            bins[key] = {'max': max, 'avg': avg};
+        }
+        this.fGain.update(bins, 'max');
+        this.fSmooth.update(bins, 'max');
+        Object.keys(bins).forEach(k => {
+            bins[k].gain = this.fGain.val[k];
+            bins[k].smooth = this.fSmooth.val[k];
+            bins[k].onset = false;
+            this.samples[k].samples.push(bins[k].max);
+            while (this.samples[k].samples.length > this.samplesLen) this.samples[k].samples.shift();
+            if (this.samples[k].ignorePeaks == 0) {
+                let avg = this.samples[k].samples.reduce((p, c) => p + c) / this.samples[k].samples.length;
+                if (bins[k].max >= Math.max(0.25, avg)) {
+                    bins[k].onset = true;
+                    this.samples[k].ignorePeaks = parseInt(this.fps / 4);
+                }
+            }
+            this.samples[k].ignorePeaks = Math.max(0, --this.samples[k].ignorePeaks);
+        });
+        events.fire('audio/analysis/data', this, bins);
     }
 
     stop(event, input) {
-        events.fire('audio/analysis/stop', this);
-        this.analyzer.stop();
-        this.analyzer = null;
+        window.clearInterval(this.interval);
+        input.source.disconnect(this.analyzer);
+        this._resetProps();
     }
 }
-
-/* basic algo:
-props: min/max tempo (60,160?), buffer size, window size (min tempo * 1.1), C=0.95?
-lpf of signal
-put chunk in rolling window w/ ts
-find peaks where peak > C * avg(window)
-histogram of distances between peaks
-confidence is 2nd highest hist / max hist
-calc tempo: 60 / (window size / sample rate)
-while tempo < min tempo, *=2
-while tempo > max tempo, /= 2
-if current block contains a peak, & distance from last peak in max hist, this is probably a beat
-likewise, if block @ now - tempo looks like a beat, probably a beat (predicted)
-*/
--- src/js/lib/ui/audiographs.js
+++ src/js/lib/ui/audiographs.js
@@ -1,223 +1,31 @@
 import * as events from '/src/js/lib/events';
-import {FEATURES} from '/src/js/lib/constants';
-import {Series, TSChart, SpectSeries, SpectChart} from '/src/js/lib/ui/components/chart';
-
-let graphs = [];
-
-function destroy_graphs() {
-    graphs.forEach(g => g.destroy());
-    graphs = [];
-}
 
 export default function setup() {
-    events.subscribe('audio/analysis/features', (ev, analyzer, opts) => {
-        destroy_graphs();
-        const timelen = (opts.sampleRate / opts.bufferSize) * 10;
-        const make_graph = function(key, title, description, cls, opts) {
-            let graph = null;
-            graphs.forEach(g => {
-                if (g.opts.key === key)
-                    graph = g;
-            });
-            if (graph === null) {
-                const config = {
-                    'key': key,
-                    'title': title,
-                    'description': description || null
-                };
-                Object.assign(config, opts || {});
-                graph = new (cls || TSChart)(document.getElementById('graphs'), config);
-                graphs.push(graph);
-            }
-            return graph;
-        };
+    let container = document.getElementById('graphs');
+    let el = document.createElement('div');
+    el.id = 'meter';
 
-        let features = {}
-        opts.features.forEach(f => {
-            let feature = {key: f};
-            Object.assign(feature, FEATURES._default, FEATURES[f]);
-            ['range', 'fftCount'].forEach(k => {
-                if (typeof feature[k] === 'function')
-                    feature[k] = feature[k](opts.sampleRate, opts.bufferSize, analyzer.mfccCoefficients);
-            });
-            features[f] = feature;
-        });
-
-        ['rms', 'energy'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('volume', 'Volume');
-            graph.addSeries(new Series(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                },
-                feature
-            ));
-        });
-
-        ['spectralFlatness', 'spectralSlope', 'spectralKurtosis'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('spectrum-shape', 'Spectrum Shape');
-            graph.addSeries(new Series(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                },
-                feature
-            ));
-        });
-
-        ['spectralCentroid', 'spectralSpread'].forEach(k => {
-            // TODO: fft-type graph
-            let feature = features[k];
-            let graph = make_graph('spectrum-spread', 'Spectrum Spread');
-            graph.addSeries(new Series(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                },
-                feature
-            ));
-        });
-
-        ['spectralRolloff'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('spectrum-rolloff', 'Spectrum Rolloff');
-            graph.addSeries(new Series(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                },
-                feature
-            ));
-        });
-
-        ['zcr', 'spectralSkewness'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('spectrum-misc', 'Spectrum');
-            graph.addSeries(new Series(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                },
-                feature
-            ));
-        });
-
-        ['perceptualSpread', 'perceptualSharpness'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('perceptual', 'Perceptual Features');
-            graph.addSeries(new Series(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                },
-                feature
-            ));
-        });
-
-        ['amplitudeSpectrum'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('amplitudeSpectrum', 'amplitudeSpectrum', false, SpectChart);
-            graph.addSeries(new SpectSeries(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                    fftCount: feature.fftCount,
-                },
-                feature
-            ));
-        });
-
-        ['powerSpectrum'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('powerSpectrum', 'powerSpectrum', false, SpectChart);
-            graph.addSeries(new SpectSeries(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                    fftCount: feature.fftCount,
-                },
-                feature
-            ));
-        });
-
-        ['mfcc'].forEach(k => {
-            let feature = features[k];
-            let graph = make_graph('mfcc', 'mfcc', false, SpectChart);
-            graph.addSeries(new SpectSeries(
-                {
-                    key: feature.key,
-                    title: feature.title,
-                    description: feature.description,
-                    color: feature.color,
-                    range: feature.range,
-                    autoRange: feature.autoRange,
-                    fftCount: feature.fftCount,
-                },
-                feature
-            ));
-        });
-
-
-        // chroma - pitch
-        // loudness - loudness
-
+    let html = '';
+    ['sub', 'bass', 'low_mid', 'mid', 'high_mid', 'presence', 'brilliance'].forEach(b => {
+        html += `<div class="bin ${b}"><div class="cover"></div><div class="bar"></div></div>`;
     });
+    el.innerHTML = html;
+    container.appendChild(el);
 
-    events.subscribe('audio/analysis/stop', destroy_graphs);
+    events.subscribe('audio/analysis/data', (ev, analyzer, data) => {
+        Object.keys(data).forEach(b => {
+            let d = data[b];
+            let bin = el.querySelector('.bin.' + b);
+            let cover = bin.querySelector('.cover');
+            let bar = bin.querySelector('.bar');
 
-    events.subscribe('audio/analysis/data', (ev, analyzer, features) => {
-        const now = (new Date()).getTime() / 1000;
-        graphs.forEach(graph => {
-            Object.keys(graph.data).forEach(k => {
-                let feature = graph.data[k].extra;
-                switch (feature.type) {
-                    case 'scalar':
-                    case 'fft':
-                        graph.push(k, features[feature.key]);
-                        break;
-                    case 'pitch':
-                        break;
-                    case 'loudness':
-                        break;
-                    default:
-                        console.error("Invalid feature type %s", config.feature.type);
-                        break;
-                }
-            });
+            bar.style.bottom = (d.gain * 100) + '%';
+            cover.style.height = ((1 - d.smooth) * 100) + '%';
+
+            if (d.onset)
+                bin.classList.add('onset');
+            else
+                bin.classList.remove('onset');
         });
     });
 }
